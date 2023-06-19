2947957e37753882b50533d4678e10a0a1e39f4b
Dark Soul <newforaccess@gmail.com>
2023-06-19 23:57:45 +0600

    added login pannel

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "splash.h"
+#include "login.h"
 
 int main()
 {
     recursiveLoading(0);   //use of recursion
     splash();
+     system("cls");
+     login();
 
     return 0;
 }
